f1e4fb119f4d4d4b29dad7335f82d99e4b904197
Влад <45657091+mamonovvlad@users.noreply.github.com>
2019-09-17 23:31:59 +0300

    tqwf23

---
+++ src/js/main.js
@@ -0,0 +1,11 @@
+$('.fade').slick({
+    dots: false,
+    infinite: true,
+    speed: 500,
+    fade: true,
+    cssEase: 'linear',
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+});
