003e28aab2fb7e2d8415e945166bf7b17af71f22
Влад <45657091+mamonovvlad@users.noreply.github.com>
2019-09-18 21:41:14 +0300

    12

--- src/js/main.js
+++ src/js/main.js
@@ -9,3 +9,23 @@ $('.fade').slick({
     autoplay: true,
     autoplaySpeed: 2000,
 });
+
+
+
+var burger = "";
+document.addEventListener("DOMContentLoaded", function() {
+  burger = document.getElementById('burger')
+  burger.onclick = toggleBurger
+});
+function toggleBurger() {
+  if(burger.classList.contains("active")){
+    burger.classList.remove("active");
+  }else{
+    burger.classList += " active";
+  }
+}
+
+
+$('main').click(function(){
+    $('.menu__links').fadeToggle()
+})
