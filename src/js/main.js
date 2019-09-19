eee92438d02fd755f334e414fe485f7ca679d30f
Влад <45657091+mamonovvlad@users.noreply.github.com>
2019-09-19 03:23:08 +0300

    few

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,29 @@
-$('.fade').slick({
+$(document).ready(function () {
+
+  $(window).on("scroll", function () {
+    var scro = $(window).scrollTop();
+    var scr = 0; // начало страницы меню студия
+    var scr2 = 200; // начало меню услуги
+    /*      положение  скрола относительно пункта Студия    */
+    if ($(window).scrollTop() >= scr && scro <= scr2) {
+      $('.menu-box__logo').removeClass('active');
+      $('.menu-box__text li a').removeClass('active');
+    } else {
+      $('.menu-box__logo').addClass('active');
+      $('.menu-box__text li a').addClass('active');
+
+      $('.menu-box__logo').css({
+        'transition': '.5s'
+      });
+      $('.menu-box__text li a').css({
+        'transition': '.5s'
+      });
+    }
+  });
+
+
+
+  $('.fade').slick({
     dots: false,
     infinite: true,
     speed: 500,
@@ -8,24 +33,28 @@ $('.fade').slick({
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 2000,
-});
+  });
 
 
 
-var burger = "";
-document.addEventListener("DOMContentLoaded", function() {
-  burger = document.getElementById('burger')
-  burger.onclick = toggleBurger
-});
-function toggleBurger() {
-  if(burger.classList.contains("active")){
-    burger.classList.remove("active");
-  }else{
-    burger.classList += " active";
+  var burger = "";
+  document.addEventListener("DOMContentLoaded", function () {
+    burger = document.getElementById('burger')
+    burger.onclick = toggleBurger
+  });
+
+  function toggleBurger() {
+    if (burger.classList.contains("active")) {
+      burger.classList.remove("active");
+    } else {
+      burger.classList += " active";
+    }
   }
-}
 
 
-$('main').click(function(){
+  $('main').click(function () {
     $('.menu__links').fadeToggle()
+  })
+
+
 })
